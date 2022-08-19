5dc889e923ceef6b1274c136fa320767a0ea14b9
Capibarista <74478133+yvira12345@users.noreply.github.com>
2022-08-19 13:32:23 -0300

    Add files via upload

--- Unloockey.py
+++ Unloockey.py
@@ -1,4 +1,3 @@
-
 from itertools import permutations
 import configparser
 
@@ -8,8 +7,11 @@ cfg.read("Unloockey.cfg")
 CONFIG = {"global": {
     "years": cfg.get("years", "years").split(","),
     "chars": cfg.get("chars", "chars").split(","),
-    "numeros": cfg.get("nums", "numeros").split(",")
-
+    "numeros": cfg.get("nums", "numeros").split(","),
+    "upp": cfg.get("config", "up"),
+    "yearsO": cfg.get("config", "years"),
+    "numsfromtoO": cfg.get("config", "numfromto"),
+    "numerosO": cfg.get("config", "numeros")
 }}
 
 birthdate = ""
@@ -20,14 +22,50 @@ numbers = []
 numeros = CONFIG["global"]["numeros"]
 years = CONFIG["global"]["years"]
 chars = CONFIG["global"]["chars"]
+upp = CONFIG["global"]["upp"]
+yearsO = CONFIG["global"]["yearsO"]
+numsfromtoO = CONFIG["global"]["numsfromtoO"]
+numerosO = CONFIG["global"]["numerosO"]
 
 # Almacena los años especificados en el archivo de configuración
-for y in years:
-    numbers.append(y)
+if yearsO == "y":
+    for y in years:
+        numbers.append(y)
+# Almacena los numeros especificados en el archivo de configuración
+if numerosO == "y":
+    for num in numeros:
+        numbers.append(num)
+
 
 # Almacena numeros del 0 al 100
-for i in range(101):
-    numbers.append(i)
+if numsfromtoO == "y":
+    for i in range(101):
+        numbers.append(i)
+
+# Agregar perfil
+with open("profile.txt") as f:
+    for line in f:
+        line = line.rstrip('\n')
+        if line.startswith("birthdate:"):
+            none, line = line.split(':')
+            if len(line) != 0 and len(line) != 8:
+                print("ERROR: INCORRECT SYNTAX Birthdate (DDMMYYYY)")
+                exit()
+            else:
+                birthdate = line
+        elif line.startswith("address:"):
+            none, line = line.split(':')
+            address = line
+
+        elif isinstance(line, str):
+            words.append(line.lower())
+            if upp == "y":
+                words.append(line.lower().title())
+
+        else:
+            numbers.append(line)
+
+
 
 # Almacena los números de la dirección en la lista de numeros,
 # Pone la primera letra de cada palabra de la dirección en mayuscula los espacios y remueve los espacios .
@@ -51,27 +89,7 @@ numbers.append(birthdate[2:4])
 words.append(''.join([i for i in address if not i.isdigit()]))
 words.append(''.join([i for i in addressup if not i.isdigit()]))
 
-# Agregar perfil
-with open("profile.txt") as f:
-    for line in f:
-        line = line.rstrip('\n')
-        if line.startswith("birthdate:"):
-            none, line = line.split(':')
-            if len(line) != 0 and len(line) != 8:
-                print("ERROR: INCORRECT SYNTAX Birthdate (DDMMYYYY)")
-                exit()
-            else:
-                birthdate = line
-        elif line.startswith("address:"):
-            none, line = line.split(':')
-            address = line
 
-        elif isinstance(line, str):
-            words.append(line)
-            print(words[0])
-        else:
-            numbers.append(line)
-            print(numbers[0])
 
 # //WORDLIST//
 
